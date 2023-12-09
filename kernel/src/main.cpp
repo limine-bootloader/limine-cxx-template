f188601409363c3a5beb025e8c0f7179eea336ee
mintsuki <mintsuki@protonmail.com>
2023-12-09 14:10:21 +0100

    Initial commit

---
+++ kernel/src/main.cpp
@@ -0,0 +1,116 @@
+#include <cstdint>
+#include <cstddef>
+#include <limine.h>
+
+// Set the base revision to 1, this is recommended as this is the latest
+// base revision described by the Limine boot protocol specification.
+// See specification for further info.
+
+LIMINE_BASE_REVISION(1)
+
+// The Limine requests can be placed anywhere, but it is important that
+// the compiler does not optimise them away, so, in C++, they should
+// NOT be made "static".
+
+limine_framebuffer_request framebuffer_request = {
+    .id = LIMINE_FRAMEBUFFER_REQUEST,
+    .revision = 0,
+    .response = NULL
+};
+
+// GCC and Clang reserve the right to generate calls to the following
+// 4 functions even if they are not directly called.
+// Implement them as the C specification mandates.
+// DO NOT remove or rename these functions, or stuff will eventually break!
+// They CAN be moved to a different .cpp file.
+
+extern "C" {
+
+void *memcpy(void *dest, const void *src, std::size_t n) {
+    std::uint8_t *pdest = static_cast<std::uint8_t *>(dest);
+    const std::uint8_t *psrc = static_cast<const std::uint8_t *>(src);
+
+    for (std::size_t i = 0; i < n; i++) {
+        pdest[i] = psrc[i];
+    }
+
+    return dest;
+}
+
+void *memset(void *s, int c, std::size_t n) {
+    std::uint8_t *p = static_cast<std::uint8_t *>(s);
+
+    for (std::size_t i = 0; i < n; i++) {
+        p[i] = static_cast<uint8_t>(c);
+    }
+
+    return s;
+}
+
+void *memmove(void *dest, const void *src, std::size_t n) {
+    std::uint8_t *pdest = static_cast<std::uint8_t *>(dest);
+    const std::uint8_t *psrc = static_cast<const std::uint8_t *>(src);
+
+    if (src > dest) {
+        for (std::size_t i = 0; i < n; i++) {
+            pdest[i] = psrc[i];
+        }
+    } else if (src < dest) {
+        for (std::size_t i = n; i > 0; i--) {
+            pdest[i-1] = psrc[i-1];
+        }
+    }
+
+    return dest;
+}
+
+int memcmp(const void *s1, const void *s2, std::size_t n) {
+    const std::uint8_t *p1 = static_cast<const std::uint8_t *>(s1);
+    const std::uint8_t *p2 = static_cast<const std::uint8_t *>(s2);
+
+    for (std::size_t i = 0; i < n; i++) {
+        if (p1[i] != p2[i]) {
+            return p1[i] < p2[i] ? -1 : 1;
+        }
+    }
+
+    return 0;
+}
+
+}
+
+// Halt and catch fire function.
+static void hcf() {
+    asm ("cli");
+    for (;;) {
+        asm ("hlt");
+    }
+}
+
+// The following will be our kernel's entry point.
+// If renaming _start() to something else, make sure to change the
+// linker script accordingly.
+extern "C" void _start() {
+    // Ensure the bootloader actually understands our base revision (see spec).
+    if (LIMINE_BASE_REVISION_SUPPORTED == false) {
+        hcf();
+    }
+
+    // Ensure we got a framebuffer.
+    if (framebuffer_request.response == NULL
+     || framebuffer_request.response->framebuffer_count < 1) {
+        hcf();
+    }
+
+    // Fetch the first framebuffer.
+    limine_framebuffer *framebuffer = framebuffer_request.response->framebuffers[0];
+
+    // Note: we assume the framebuffer model is RGB with 32-bit pixels.
+    for (std::size_t i = 0; i < 100; i++) {
+        volatile std::uint32_t *fb_ptr = static_cast<volatile std::uint32_t *>(framebuffer->address);
+        fb_ptr[i * (framebuffer->pitch / 4) + i] = 0xffffff;
+    }
+
+    // We're done, just hang...
+    hcf();
+}
