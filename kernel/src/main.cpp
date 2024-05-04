67564c4771bed533e20c1fe4818ebfe8261441ef
mintsuki <mintsuki@protonmail.com>
2024-05-04 02:30:46 +0200

    Updates for Limine 7.5.0 and requests delimiters

--- kernel/src/main.cpp
+++ kernel/src/main.cpp
@@ -2,24 +2,43 @@
 #include <cstddef>
 #include <limine.h>
 
-// Set the base revision to 1, this is recommended as this is the latest
+// Set the base revision to 2, this is recommended as this is the latest
 // base revision described by the Limine boot protocol specification.
 // See specification for further info.
 
-namespace { volatile LIMINE_BASE_REVISION(1); }
+namespace {
+
+__attribute__((used, section(".requests")))
+volatile LIMINE_BASE_REVISION(2);
+
+}
 
 // The Limine requests can be placed anywhere, but it is important that
 // the compiler does not optimise them away, so, usually, they should
 // be made volatile or equivalent, _and_ they should be accessed at least
-// once.
+// once or marked as used with the "used" attribute as done here.
 
 namespace {
 
+__attribute__((used, section(".requests")))
 volatile limine_framebuffer_request framebuffer_request = {
     .id = LIMINE_FRAMEBUFFER_REQUEST,
     .revision = 0,
     .response = nullptr
 };
+
+}
+
+// Finally, define the start and end markers for the Limine requests.
+// These can also be moved anywhere, to any .cpp file, as seen fit.
+
+namespace {
+
+__attribute__((used, section(".requests_start_marker")))
+volatile LIMINE_REQUESTS_START_MARKER;
+
+__attribute__((used, section(".requests_end_marker")))
+volatile LIMINE_REQUESTS_END_MARKER;
 
 }
 
