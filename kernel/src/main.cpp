22b33c3cb7a0f3e479af3915bff3441fb90d36f8
mintsuki <mintsuki@protonmail.com>
2024-06-02 18:49:36 +0200

    Add __dso_handle

--- kernel/src/main.cpp
+++ kernel/src/main.cpp
@@ -115,13 +115,14 @@ void hcf() {
 
 }
 
-// The following two stubs are required by the Itanium C++ ABI (the one we use,
+// The following stubs are required by the Itanium C++ ABI (the one we use,
 // regardless of the "Itanium" nomenclature).
 // Like the memory functions above, these stubs can be moved to a different .cpp file,
-// but should never be removed.
+// but should not be removed, unless you know what you are doing.
 extern "C" {
     int __cxa_atexit(void (*)(void *), void *, void *) { return 0; }
     void __cxa_pure_virtual() { hcf(); }
+    void *__dso_handle;
 }
 
 // Extern declarations for global constructors array.
