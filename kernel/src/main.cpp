eb7f1a110450877a5f41ca3ec83457319e24a969
Mintsuki <mintsuki@protonmail.com>
2026-03-23 14:50:49 +0100

    Update to Limine 11.x and base revision 6

--- kernel/src/main.cpp
+++ kernel/src/main.cpp
@@ -2,14 +2,14 @@
 #include <cstddef>
 #include <limine.h>
 
-// Set the base revision to 5, this is recommended as this is the latest
+// Set the base revision to 6, this is recommended as this is the latest
 // base revision described by the Limine boot protocol specification.
 // See specification for further info.
 
 namespace {
 
 __attribute__((used, section(".limine_requests")))
-volatile std::uint64_t limine_base_revision[] = LIMINE_BASE_REVISION(5);
+volatile std::uint64_t limine_base_revision[] = LIMINE_BASE_REVISION(6);
 
 }
 
