2d010c86bd2e26140a616bccd3d5bce29102a42d
Mintsuki <mintsuki@protonmail.com>
2026-09-12 02:31:10 +0200

    Fix the comment referring to the memory functions
    Signed-off-by: Mintsuki <mintsuki@protonmail.com>

--- kernel/src/main.cpp
+++ kernel/src/main.cpp
@@ -61,7 +61,7 @@ void hcf() {
 
 // The following stubs are required by the Itanium C++ ABI (the one we use,
 // regardless of the "Itanium" nomenclature).
-// Like the memory functions above, these stubs can be moved to a different .cpp file,
+// Like the memory functions in memory.cpp, these stubs can be moved to a different .cpp file,
 // but should not be removed, unless you know what you are doing.
 extern "C" {
     int __cxa_atexit(void (*)(void *), void *, void *) { return 0; }
